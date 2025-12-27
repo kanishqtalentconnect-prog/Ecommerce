f19dab7d2ba89e2980c32ac3556f2cf14c9988ed
kanishqtalentconnect-prog <kanishq.talentconnect@gmail.com>
2025-12-27 15:45:14 +0530

    Hosted Backend

--- C_frontend/src/lib/axios.js
+++ C_frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000", 
+    baseURL: import.meta.env.VITE_API_URL || "https://ecommerce-3pxk.onrender.com/", 
     withCredentials: true, 
 });
 
